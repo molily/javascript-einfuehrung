21ee3d579c2c3c1783d7ce424426018978174b40
Mathias Schaefer (molily) <zapperlott@gmail.com>
2010-02-05 16:11:03 +0100

    Changes

--- js-doku.js
+++ js-doku.js
@@ -28,12 +28,49 @@ function ContentLoaded(c,j){var b=c.document,h='DOMContentLoaded',d=c.navigator.
 
 var CoolURIs = {
 	rules : {
-		'einbindung.html#fehler-code-als-string' : 'event-handling-grundlagen.html#fehler-code-als-string'
+		'einbindung.html#ereignisbasierung' : 'event-handling-grundlagen.html#ereignisbasierung',
+		'einbindung.html#phase-laden' : 'event-handling-grundlagen.html#phase-laden',
+		'einbindung.html#phase-onload' : 'event-handling-grundlagen.html#phase-onload',
+		'einbindung.html#phase-event-handling' : 'event-handling-grundlagen.html#phase-event-handling',
+		'einbindung.html#script-struktur' : 'event-handling-grundlagen.html#script-struktur',
+		'einbindung.html#traditionelles-event-handling' : 'event-handling-grundlagen.html#traditionelles-event-handling',
+		'einbindung.html#traditionelles-schema' : 'event-handling-grundlagen.html#traditionelles-schema',
+		'einbindung.html#handler-loeschen' : 'event-handling-grundlagen.html#handler-loeschen',
+		'einbindung.html#fehler-handler-aufrufen' : 'event-handling-grundlagen.html#fehler-handler-aufrufen',
+		'einbindung.html#inline-handler' : 'event-handling-grundlagen.html#inline-handler',
+		'einbindung.html#fehler-code-als-string' : 'event-handling-grundlagen.html#fehler-code-als-string',
+		'einbindung.html#event-objekt' : 'event-handling-objekt.html#event-objekt',
+		'einbindung.html#standardaktion' : 'event-handling-objekt.html#standardaktion',
+		'einbindung.html#bubbling' : 'event-handling-objekt.html#bubbling',
+		'einbindung.html#currenttarget-target' : 'event-handling-objekt.html#currenttarget-target',
+		'einbindung.html#bubbling-verhindern' : 'event-handling-objekt.html#bubbling-verhindern',
+		'einbindung.html#traditionell-nachteile' : 'event-handling-fortgeschritten.html#traditionell-nachteile',
+		'einbindung.html#dom-events' : 'event-handling-fortgeschritten.html#dom-events',
+		'einbindung.html#addEventListener' : 'event-handling-fortgeschritten.html#addEventListener',
+		'einbindung.html#removeEventListener' : 'event-handling-fortgeschritten.html#removeEventListener',
+		'einbindung.html#microsoft' : 'event-handling-fortgeschritten.html#microsoft',
+		'einbindung.html#attachEvent' : 'event-handling-fortgeschritten.html#attachEvent',
+		'einbindung.html#detachEvent' : 'event-handling-fortgeschritten.html#detachEvent',
+		'einbindung.html#microsoft-eigenheiten' : 'event-handling-fortgeschritten.html#microsoft-eigenheiten',
+		'einbindung.html#addevent' : 'event-handling-fortgeschritten.html#addevent',
+		'einbindung.html#addevent-lage' : 'event-handling-fortgeschritten.html#addevent-lage',
+		'einbindung.html#addevent-helfer' : 'event-handling-fortgeschritten.html#addevent-helfer',
+		'einbindung.html#einfaches-addevent' : 'event-handling-fortgeschritten.html#einfaches-addevent',
+		'einbindung.html#flexibles-addevent' : 'event-handling-fortgeschritten.html#flexibles-addevent',
+		'einbindung.html#addevent-frameworks' : 'event-handling-fortgeschritten.html#addevent-frameworks',
+		'einbindung.html#onload' : 'event-handling-onload.html#onload',
+		'einbindung.html#onload-nachteile' : 'event-handling-onload.html#onload-nachteile',
+		'einbindung.html#domcontentloaded' : 'event-handling-onload.html#domcontentloaded',
+		'einbindung.html#domcontentloaded-crossbrowser' : 'event-handling-onload.html#domcontentloaded-crossbrowser',
+		'einbindung.html#domcontentloaded-nachteile' : 'event-handling-onload.html#domcontentloaded-nachteile',
+		'einbindung.html#delegation' : 'event-handling-effizient.html#delegation',
+		'einbindung.html#capturing' : 'event-handling-effizient.html#capturing'
 	},
 	init : function () {
 		var currentURI = location.href, newURI;
 		for (var oldURI in CoolURIs.rules) {
-			if (currentURI.indexOf(oldURI) > -1) {
+			var regExp = new RegExp(oldURI.replace('.', '\\.') + "$");
+			if (regExp.test(currentURI)) {
 				location.href = CoolURIs.rules[oldURI];
 				break;
 			}
